d576901e1c0a4078d06035d93d8519157c20d004
JorisWessels <joris.wessels@hotmail.com>
2020-09-25 23:57:06 +0200

    Added solution: Quality-Adjusted Life-Year

---
+++ Quality-Adjusted Life-Year/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Quality_Adjusted_Life_Year
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(CalculateAccumulated(Convert.ToInt32(Console.ReadLine())));
+        }
+
+        private static decimal CalculateAccumulated(int periods)
+        {
+            decimal result = 0;
+            for (var i = 0; i < periods; i++) {
+                var input = Console.ReadLine();
+                if (input != null) {
+                    var doubleStrings = input.Split(' ');
+                    result += decimal.Parse(doubleStrings[0]) * decimal.Parse(doubleStrings[1]);
+                }
+            }
+
+            return result;
+        }
+    }
+}
