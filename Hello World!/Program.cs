74d5ccd9a7365e615d6efd5a40c35ad9af03a967
JorisWessels <joris.wessels@hotmail.com>
2020-09-25 23:52:36 +0200

    Added solution: Hello World!

---
+++ Hello World!/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello_World_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
