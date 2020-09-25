562739f2fc243ba1e154042d1370e974136f12a7
JorisWessels <joris.wessels@hotmail.com>
2020-09-25 23:58:09 +0200

    Added solution: Pot

---
+++ Pot/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Pot
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(Calculate(Convert.ToInt32(Console.ReadLine())));
+        }
+
+        public static int Calculate(int numberOfAddends)
+        {
+            var result = 0;
+            for (var i = 0; i < numberOfAddends; i++) {
+                var input = Convert.ToInt32(Console.ReadLine());
+                result += (int) Math.Pow(input / 10, input % 10);
+            }
+
+            return result;
+        }
+    }
+}
