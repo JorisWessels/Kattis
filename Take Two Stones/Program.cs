834127f68a852de2322f13969c8738cef7ed884a
JorisWessels <joris.wessels@hotmail.com>
2020-09-25 23:54:36 +0200

    Added solution: Take Two Stones

---
+++ Take Two Stones/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Take_Two_Stones
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(CheckWinner(Convert.ToInt32(Console.ReadLine())));
+        }
+
+        private static string CheckWinner(int input)
+        {
+            if (input % 2 != 0) {
+                return "Alice";
+            }
+
+            return "Bob";
+        }
+    }
+}
