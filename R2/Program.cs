38354f095fa568e56f2e67401c4f1a90e2b625ed
JorisWessels <joris.wessels@hotmail.com>
2020-09-25 23:55:39 +0200

    Added solution: R2

---
+++ R2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace R2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(RestoreBirthdayPresent(Console.ReadLine()));
+        }
+
+        public static int RestoreBirthdayPresent(string input)
+        {
+            var intStrings = input.Split(' ');
+            return Convert.ToInt32(intStrings[1]) * 2 - Convert.ToInt32(intStrings[0]);
+        }
+    }
+}
