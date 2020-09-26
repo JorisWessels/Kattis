7f3b012ffccc7b85445eda27f3e2675337f3d53b
JorisWessels <joris.wessels@hotmail.com>
2020-09-26 14:04:09 +0200

    Added solution: Faktor

---
+++ Faktor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Faktor
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(CalcImpactFactor(Console.ReadLine()));
+        }
+
+        public static int CalcImpactFactor(string numberOfArticles)
+        {
+            var intStrings = numberOfArticles.Split(' ');
+            return (int) (Convert.ToInt32(intStrings[0]) * (Convert.ToInt32(intStrings[1]) - 0.99) + 1);
+        }
+    }
+}
