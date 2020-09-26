516d7e416f74964c03d923502319f25fbc46212f
JorisWessels <joris.wessels@hotmail.com>
2020-09-26 18:19:37 +0200

    Added solution: Planina

---
+++ Planina/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Planina
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(CalcNumberOfPoints(Convert.ToInt32(Console.ReadLine())));
+        }
+
+        public static int CalcNumberOfPoints(int numberOfIterations)
+        {
+            var length = 2;
+            var increaseLengthBy = 1;
+
+            for (var i = 0; i < numberOfIterations; i++) {
+                length += increaseLengthBy;
+                increaseLengthBy += increaseLengthBy;
+            }
+
+            return length * length;
+        }
+    }
+}
