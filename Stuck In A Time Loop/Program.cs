2f4f7d7575f1d9fb3b842ff275de48fff19c84c0
JorisWessels <joris.wessels@hotmail.com>
2020-09-30 18:09:42 +0200

    Added solution: Stuck In A Time Loop

---
+++ Stuck In A Time Loop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Stuck_In_A_Time_Loop
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            SayAbracadabra(Convert.ToInt32(Console.ReadLine()));
+        }
+
+        public static void SayAbracadabra(int amount)
+        {
+            for (var i = 0; i < amount; i++) {
+                Console.WriteLine($"{i + 1} Abracadabra");
+            }
+        }
+    }
+}
